3972d423565aa8cc35ccc9847c7b45164be08982
Ilya <i.fomin@adictgroup.ru>
2026-01-27 16:35:37 +0800

    feat: добавляет основной код бота

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface UserData {
+    id: number;
+    username?: string;
+    socialRating: number;
+    messagesCount: number;
+}
+
+export interface Vote {
+    targetId: number;
+    amount: number;
+    votes: { [userId: number]: boolean }; // true - за, false - против
+    creatorId: number;
+}
+
+export interface BotConfig {
+    trackedWord: string;
+    rewardPoints: number;
+    rewardSticker: string; // стикер в ответ на слово
+    triggerStickerId: string; // стикер от пользователя
+    botResponseStickerId: string; // стикер в ответ на стикер пользователя
+    weeklyWordCount: { [word: string]: number };
+    weeklyStickerCount: number;
+}
+
+export interface Database {
+    users: { [id: number]: UserData };
+    config: BotConfig;
+}
